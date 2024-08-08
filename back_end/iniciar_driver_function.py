9f114236521597bf25727cba5936b2b98d13e1a9
João Pedro Souza Barbosa <joaopedro150308@gmail.com>
2024-08-08 19:56:45 -0300

    Implemented venv

--- back_end/iniciar_driver_function.py
+++ back_end/iniciar_driver_function.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import *
